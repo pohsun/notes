022bd824226969c2758810d194ea46470058f45a
pohsun <pohsun.chen.hep@gmail.com>
2022-12-31 16:58:35 +0800

    vault backup: 2022-12-31 16:58:35
    Affected files:
    Playground/sixconn/xapp.py

--- Playground/sixconn/xapp.py
+++ Playground/sixconn/xapp.py
@@ -4,6 +4,9 @@
 
 from __future__ import print_function, division
 
+import sys
+import copy
+
 # py2, py3 compatible abc.
 # https://stackoverflow.com/a/38668373/2008784
 import abc
@@ -12,28 +15,85 @@ ABC = abc.ABCMeta('ABC', (object,), {'__slots__': ()})
 import threading
 
 
-class XServerStates(object):
-    INIT = 0
-    READY = 1
-    RUN = 2
-    TERMINATED = 3
-    FAULT = -1
+class XAppStates(object):
+    INIT = copy.copy(0)
+    READY = copy.copy(1)
+    RUN = copy.copy(2)
+    TERMINATED = copy.copy(3)
+    FAULT = copy.copy(-1)
+
+
+class XAppFSM(object):
+    def __init__(self):
+        self._state = XAppStates.INIT
+
+    @property
+    def state(self):
+        return self._state
+
+    def to_ready(self):
+        pass
+
+    def to_run(self):
+        pass
+
+    def to_terminated(self):
+        pass
+
+    def to_fault(self):
+        pass
+    
 
 class AbsXServerApp(ABC):
     def __init__(self, server):
         self._server = server  # type: socketserver.TCPServer
+        self._fsm = XAppFSM()
+        self._runThread = None
 
     def echo(self, args, kwargs):
         return (args, kwargs)
 
-    @abc.abstractmethod
-    def setup(self):
+    def setup(self, args, kwargs):
+        output = self._setup(*args, **kwargs)
+        self._state = 
+        return output
+
+    def _setup(self):
         pass
 
-    @abc.abstractmethod
-    def run()
+    def run(self, args, kwargs):
+        def autoexit_run(*args, **kwargs):
+            try:
+                self._run(*args, **kwargs)
+            finally:
+                # As `autoexit_run` is called in a thread, only the thread exits.
+                sys.exit()
+        self._runT = threading.Thread(
+            target=autoexit_run,
+            args=args,
+            kwargs=kwargs)
+        self._runT.start()
 
-    def poll(self):
+    @abc.abstractmethod
+    def _run(self, *args, **kwargs)
+        raise NotImplementedError
+
+    def join(self, timeout=None):
+        """ This method returns True just before the run() method starts until just after the run() method terminates. """
+        try:
+            self._runT.join(timeout=timeout)
+        except RuntimeError:
+            # join() raises a RuntimeError if an attempt is made to join the
+            # current thread as that would cause a deadlock. It is also an
+            # error to join() a thread before it has been started and attempts
+            # to do so raises the same exception.
+            pass
+        return self.is_alive()
+
+    def finish(self, args, kwargs):
+        return self._finish(*args, **kwargs)
+    
+    def _finish(self):
         pass
 
     def shutdown(self):
