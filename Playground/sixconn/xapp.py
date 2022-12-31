2067168e4cbf154f220686afd845faa67f66a0bc
pohsun <pohsun.chen.hep@gmail.com>
2022-12-31 16:15:30 +0800

    vault backup: 2022-12-31 16:15:29
    Affected files:
    Playground/sixconn/xapp.py

--- Playground/sixconn/xapp.py
+++ Playground/sixconn/xapp.py
@@ -4,14 +4,6 @@
 
 from __future__ import print_function, division
 
-import sys
-if sys.version_info[0] == 3:
-    import xmlrpc.server as rpcserver
-    import xmlrpc.client as rpcclient
-else:
-    import SimpleXMLRPCServer as rpcserver
-    import xmlrpclib as rpcclient
-
 # py2, py3 compatible abc.
 # https://stackoverflow.com/a/38668373/2008784
 import abc
@@ -29,37 +21,20 @@ class XServerStates(object):
 
 class AbsXServerApp(ABC):
     def __init__(self, server):
-        self._server = server
-        self._counter = 0
+        self._server = server  # type: socketserver.TCPServer
 
     def echo(self, args, kwargs):
-        self._counter += 1
-        print(self._counter)
         return (args, kwargs)
+
+    @abc.abstractmethod
+    def setup(self):
+        pass
+
+    @abc.abstractmethod
+    def run()
 
     def poll(self):
         pass
 
     def shutdown(self):
-        threading.Thread(target=server.shutdown).start()
-
-
-if __name__ == '__main__':
-    server = rpcserver.SimpleXMLRPCServer(
-            ("127.0.0.1", 54321),
-            allow_none=True)
-    app = AbsXServerApp(server)
-    server.register_instance(app)
-
-    server.register_introspection_functions()
-    t = threading.Thread(target=server.serve_forever)
-    t.start()
-
-    cli = rpcclient.ServerProxy(
-        "http://{}:{}".format(*server.server_address),
-        allow_none=True)
-    print(cli.system.listMethods())
-    print(cli.echo((None, 1, 2), {"A": 1, "B": 2}))
-    print(cli.echo((None, 1, 2), {"A": 1, "B": 2}))
-    print(cli.echo((None, 1, 2), {"A": 1, "B": 2}))
-    cli.shutdown()
+        threading.Thread(target=self._server.shutdown).start()
