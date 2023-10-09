a64f20da450f8cf4ab5998cee94f9b54b1ba9a3a
pohsun <pohsun.chen.hep@gmail.com>
2023-10-09 14:01:39 +0800

    vault backup: 2023-10-09 14:01:39
    Affected files:
    CS/CoreSkills/SICP/exercise/exercise_1_1.hs

--- CS/CoreSkills/SICP/exercise/exercise_1_1.hs
+++ CS/CoreSkills/SICP/exercise/exercise_1_1.hs
@@ -1,15 +1,30 @@
---vim: set sts=4 sw=4 noet:
+-- vim: set sts=2 sw=2 noet:
 
 main :: IO ()
 main = do
-    let showEval x = do
-        putStrLn (show x)
+  let showEval :: Int -> IO ()
+	showEval expr = do
+	  let result = expr
+	  putStrLn (show result)
 
-    showEval 10
-    showEval ((+) 5 3 4)
-    --showEval ((-) 10 1)
-    --showEval ((/) 6 2)
-    --showEval ((+) ((*) 2 4) ((-) 4 6))
-    --let a = 3
-    --let b = ((+) a 1)
-    --showEval b
+  showEval 10
+  showEval ((+) 5 3 4)
+--  --showEval ((-) 10 1)
+--  --showEval ((/) 6 2)
+--  --showEval ((+) ((*) 2 4) ((-) 4 6))
+--  --let a = 3
+--  --let b = ((+) a 1)
+--  --showEval b
+
+
+--main :: IO ()
+--main = do
+--    let printAndDouble :: Int -> IO ()
+--        printAndDouble x = do
+--            putStrLn ("Received: " ++ show x)
+--            let result = x * 2
+--            putStrLn ("Result: " ++ show result)
+--    putStrLn "Starting..."
+--    printAndDouble 5
+--    putStrLn "Done."
+
